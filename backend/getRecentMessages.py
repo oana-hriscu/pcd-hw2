f475e5c5cf9c96ab328c63c0bedf66483d3c8f84
teodoragrosu <teogrosu29@gmail.com>
2020-03-24 18:14:17 +0200

    Update getRecentMessages lambda function

---
+++ backend/getRecentMessages.py
@@ -0,0 +1,48 @@
+import boto3
+import logging
+import time
+import json
+
+logger = logging.getLogger("handler_logger")
+logger.setLevel(logging.DEBUG)
+
+dynamodb = boto3.resource("dynamodb")
+
+
+def _get_response(status_code, body):
+    if not isinstance(body, str):
+        body = json.dumps(body)
+    return {"statusCode": status_code, "body": body}
+
+
+def _send_to_connection(connection_id, data, event):
+    gatewayapi = boto3.client("apigatewaymanagementapi",
+            endpoint_url = "https://" + event["requestContext"]["domainName"] +
+                    "/" + event["requestContext"]["stage"])
+    return gatewayapi.post_to_connection(Data=json.dumps(data).encode('utf-8'), ConnectionId=connection_id)
+
+
+def get_recent_messages(event, context):
+    """
+    Return ultimele 10 mesaje
+    """
+    logger.info("Retrieving most recent messages.")
+    connectionID = event["requestContext"].get("connectionId")
+
+    # Get the 10 most recent chat messages
+    table = dynamodb.Table("serverless-chat_Messages")
+    response = table.query(KeyConditionExpression="Room = :room",
+            ExpressionAttributeValues={":room": "general"},
+            Limit=10, ScanIndexForward=False)
+    items = response.get("Items", [])
+
+    # Extract the relevant data and order chronologically
+    messages = [{"username": x["Username"], "content": x["Content"]}
+            for x in items]
+    messages.reverse()
+
+    # Send them to the client who asked for it
+    data = {"messages": messages}
+    _send_to_connection(connectionID, data, event)
+
+    return _get_response(200, "Sent recent messages.")
